3509c84bcb2757b6d773883e7e526208d1766ce4
lubinronald36-design <lubinronald36@gmail.com>
2025-11-19 11:34:20 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,44 +1,31 @@
-const questions = [
-    { question: "I don't have no money.", answer: "I don't have any money." },
-    // Add the remaining 49 questions here...
-];
-
-let currentQuestion = 0;
-let userAnswers = [];
-
-document.getElementById("question").innerText = questions[currentQuestion].question;
-document.getElementById("progress").innerText = Question ${currentQuestion + 1} of ${questions.length};
+function showResults() {
+  document.querySelector(".question-container").style.display = "none";
+  document.querySelector(".progress-container").style.display = "none";
+  document.querySelector(".results-container").style.display = "block";
+  const resultsList = document.getElementById("results");
+  questions.forEach((question, index) => {
+    const listItem = document.createElement("li");
+    listItem.innerText = Question ${index + 1}: ${question.question} | Your answer: ${userAnswers[index]} | Correct answer: ${question.answer};
+    resultsList.appendChild(listItem);
+  });
+}
 
 document.getElementById("submit").addEventListener("click", () => {
-    const userAnswer = document.getElementById("answer").value;
-    userAnswers.push(userAnswer);
-    document.getElementById("answer").value = "";
-    if (currentQuestion < questions.length - 1) {
-        currentQuestion++;
-        document.getElementById("question").innerText = questions[currentQuestion].question;
-        document.getElementById("progress").innerText = Question ${currentQuestion + 1} of ${questions.length};
-    } else {
-        showResults();
-    }
+  userAnswers.push(getUserAnswer());
+  if (currentQuestion < questions.length - 1) {
+    currentQuestion++;
+    displayQuestion();
+  } else {
+    showResults();
+  }
 });
 
 document.getElementById("next").addEventListener("click", () => {
-    if (currentQuestion < questions.length - 1) {
-        currentQuestion++;
-        document.getElementById("question").innerText = questions[currentQuestion].question;
-        document.getElementById("progress").innerText = Question ${currentQuestion + 1} of ${questions.length};
-        document.getElementById("answer").value = "";
-    }
+  if (currentQuestion < questions.length - 1) {
+    userAnswers.push(getUserAnswer());
+    currentQuestion++;
+    displayQuestion();
+  }
 });
 
-function showResults() {
-    document.querySelector(".question-container").style.display = "none";
-    document.querySelector(".progress-container").style.display = "none";
-    document.querySelector(".results-container").style.display = "block";
-    const resultsList = document.getElementById("results");
-    questions.forEach((question, index) => {
-        const listItem = document.createElement("li");
-        listItem.innerText = Question ${index + 1}: ${question.question} | Your answer: ${userAnswers[index]} | Correct answer: ${question.answer};
-        resultsList.appendChild(listItem);
-    });
-}
+displayQuestion();
